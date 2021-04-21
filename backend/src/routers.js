05c1b052b8053b04b2b5616e57e3a7763f1282be
Sean Lim <seanlimdev856@gmail.com>
2021-04-21 20:59:11 +0800

    backend: set cookie permissions

--- backend/src/routers.js
+++ backend/src/routers.js
@@ -15,56 +15,60 @@ users.use(bodyParser());
 
 // test endpoint
 publik.get('/hello', async (ctx, next) => {
-    ctx.status = 200;
-    ctx.message = 'world';
-    return;
+  ctx.status = 200;
+  ctx.message = 'world';
+  return;
 });
 
 // authenticates a user
 users.post('/login', async (ctx, next) => {
-    // extract params
-    const { username, password } = ctx.request.body;
-    // query against user email
-    const user = await ctx.db.user.findUnique({
-        where: {
-            username,
-        },
-    });
-    if (!user) {
-        ctx.body = { error: { message: 'Invalid username or password.' } };
-        ctx.status = 401;
-        return;
-    }
-    //  authenticate password
-    const passwordValid = await bcrypt.compare(password, user.password);
-    if (!passwordValid) {
-        ctx.body = { error: { message: 'Invalid username or password.' } };
-        ctx.status = 401;
-        return;
-    }
-    // create, sign, and set jwt
-    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET);
-    ctx.cookies.set('token', token);
-    // remove sensitive fields and return json data
-    delete user.password;
-
-    ctx.body = { data: user };
-    ctx.status = 200;
+  // extract params
+  const { username, password } = ctx.request.body;
+  // query against user email
+  const user = await ctx.db.user.findUnique({
+    where: {
+      username,
+    },
+  });
+  if (!user) {
+    ctx.body = { error: { message: 'Invalid username or password.' } };
+    ctx.status = 401;
     return;
+  }
+  //  authenticate password
+  const passwordValid = await bcrypt.compare(password, user.password);
+  if (!passwordValid) {
+    ctx.body = { error: { message: 'Invalid username or password.' } };
+    ctx.status = 401;
+    return;
+  }
+  // create, sign, and set jwt
+  const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET);
+  ctx.cookies.set('token', token, {
+    httpOnly: false,
+    sameSite: 'none',
+    secure: true,
+  });
+  // remove sensitive fields and return json data
+  delete user.password;
+
+  ctx.body = { data: user };
+  ctx.status = 200;
+  return;
 });
 
 users.post('/logout', async (ctx, next) => {
-    ctx.cookies.set('token', null);
-    ctx.body = { data: { message: 'Logged out successfully' } };
-    ctx.status = 200;
-    return;
+  ctx.cookies.set('token', null);
+  ctx.body = { data: { message: 'Logged out successfully' } };
+  ctx.status = 200;
+  return;
 });
 
 // fetches medical statuses from tracking Sheets document
 medicalStatus.get('/medical-statuses', async (ctx, next) => {
-    const medicalStatuses = await fetchMedicalStatuses();
-    ctx.type = 'application/json';
-    ctx.status = 200;
-    ctx.body = { data: medicalStatuses };
-    return;
+  const medicalStatuses = await fetchMedicalStatuses();
+  ctx.type = 'application/json';
+  ctx.status = 200;
+  ctx.body = { data: medicalStatuses };
+  return;
 });
--- backend/src/seeds.js
+++ backend/src/seeds.js
@@ -1,30 +1,30 @@
 import bcrypt from 'bcryptjs';
 
 export async function seedM39S(db) {
-    const adminUser = await db.user.findUnique({
-        where: {
-            username: 'm39s',
+  const adminUser = await db.user.findUnique({
+    where: {
+      username: 'm39s',
+    },
+  });
+
+  if (!adminUser) {
+    // create password hash
+    const salt = await bcrypt.genSalt(4);
+    const passwordHash = await bcrypt.hash(process.env.M39S_PASSWORD, salt);
+    // insert user record
+    try {
+      await db.user.create({
+        data: {
+          username: 'm39s',
+          password: passwordHash,
         },
-    });
-
-    if (!adminUser) {
-        // create password hash
-        const salt = await bcrypt.genSalt(4);
-        const passwordHash = await bcrypt.hash(process.env.M39S_PASSWORD, salt);
-        // insert user record
-        try {
-            await db.user.create({
-                data: {
-                    username: 'm39s',
-                    password: passwordHash,
-                },
-            });
-            console.info('Seeded m39s user');
-        } catch (error) {
-            console.error(error);
-            throw new Error('Failed to create m39s user');
-        }
+      });
+      console.info('Seeded m39s user');
+    } catch (error) {
+      console.error(error);
+      throw new Error('Failed to create m39s user');
     }
+  }
 
-    console.info('M39s admin user set up');
+  console.info('M39s admin user set up');
 }
