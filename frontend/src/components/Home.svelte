8a9d275f5d04d91434541364567c87e4783b45ae
Sean Lim <seanlimdev856@gmail.com>
2021-05-18 19:23:44 +0800

    frontend: redirect to home if not authenticated

--- frontend/src/components/Home.svelte
+++ frontend/src/components/Home.svelte
@@ -1,19 +1,27 @@
 <script>
-    import { Link, navigate } from 'svelte-routing';
+  import { Link, navigate } from 'svelte-routing';
+  import { token } from '../stores';
 
-    import fetch from '../fetch';
+  import fetch from '../fetch';
+  import { onMount } from 'svelte';
 
-    const onLogoutClicked = (e) => {
-        e.preventDefault();
-        fetch('logout', {
-            method: 'POST',
-        })
-            .then((res) => navigate('/login', { replace: true }))
-            .catch(console.error);
-    };
+  const onLogoutClicked = (e) => {
+    e.preventDefault();
+    fetch('logout', {
+      method: 'POST',
+    })
+      .then((res) => navigate('/login', { replace: true }))
+      .catch(console.error);
+  };
+
+  onMount(() => {
+    if ($token === null || $token === '') {
+      navigate('/login', { replace: true });
+    }
+  });
 </script>
 
 <div>
-    <Link to="/medical-status">Medical Statuses</Link>
-    <button on:click={onLogoutClicked}>logout</button>
+  <Link to="/medical-status">Medical Statuses</Link>
+  <button on:click={onLogoutClicked}>logout</button>
 </div>
--- frontend/src/stores.js
+++ frontend/src/stores.js
@@ -1,2 +1,30 @@
 import { writable } from 'svelte/store';
-export const token = writable('');
+
+const storage = {
+  getItem: (key) => localStorage.getItem(key),
+  setItem: (key, val) => localStorage.setItem(key, val),
+  removeItem: (key) => localStorage.removeItem(key),
+};
+
+const createTokenStore = () => {
+  const localTokenStore = storage.getItem('token');
+
+  const { subscribe, set, update } = writable(localTokenStore);
+
+  return {
+    subscribe,
+    set: (token) => {
+      storage.setItem('token', token);
+      set(token);
+    },
+    unset: () => {
+      storage.removeItem('token');
+      set(null);
+    },
+    update: (token) => {
+      storage.setItem('token', token);
+      update(token);
+    },
+  };
+};
+export const token = createTokenStore();
